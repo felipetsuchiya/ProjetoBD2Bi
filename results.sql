1e14f38caf19a376a8eb513d9f7b436bd45463a0
Felipe Tsuchiya <63445644+felipetsuchiya@users.noreply.github.com>
2020-11-09 02:25:33 -0300

    Add files via upload

--- results.sql
+++ results.sql
@@ -1,17 +1,30 @@
 a)
-SELECT empregado.nome FROM empregado  JOIN trabalha ON trabalha.codEmpregado = empregado.codEmpregado JOIN companhia ON companhia.codCompanhia = trabalha.codCompanhia WHERE companhia.nome="Casas Bahia";
+SELECT empregado.nome FROM empregado  
+JOIN trabalha ON trabalha.codEmpregado = empregado.codEmpregado 
+JOIN companhia ON companhia.codCompanhia = trabalha.codCompanhia WHERE companhia.nome="Casas Bahia";
 
 b)
-SELECT empregado.nome, empregado.cidade FROM empregado  JOIN trabalha ON trabalha.codEmpregado = empregado.codEmpregado JOIN companhia ON companhia.codCompanhia = trabalha.codCompanhia WHERE companhia.nome="GVT";
+SELECT empregado.nome, empregado.cidade FROM empregado  
+JOIN trabalha ON trabalha.codEmpregado = empregado.codEmpregado 
+JOIN companhia ON companhia.codCompanhia = trabalha.codCompanhia WHERE companhia.nome="GVT";
 
 c)
-SELECT empregado.nome, empregado.cidade, empregado.rua FROM empregado  JOIN trabalha ON trabalha.codEmpregado = empregado.codEmpregado JOIN companhia ON companhia.codCompanhia = trabalha.codCompanhia WHERE companhia.nome="TIM" AND trabalha.salario >= 3000;
+SELECT empregado.nome, empregado.cidade, empregado.rua FROM empregado  
+JOIN trabalha ON trabalha.codEmpregado = empregado.codEmpregado 
+JOIN companhia ON companhia.codCompanhia = trabalha.codCompanhia 
+WHERE companhia.nome="TIM" AND trabalha.salario >= 3000;
 
 d)
-SELECT empregado.nome, companhia.nome FROM empregado  JOIN trabalha ON trabalha.codEmpregado = empregado.codEmpregado JOIN companhia ON companhia.codCompanhia = trabalha.codCompanhia WHERE companhia.nome!="Casas Bahia";
+SELECT empregado.nome, companhia.nome FROM empregado  
+JOIN trabalha ON trabalha.codEmpregado = empregado.codEmpregado 
+JOIN companhia ON companhia.codCompanhia = trabalha.codCompanhia 
+WHERE companhia.nome!="Casas Bahia";
 
 e)
-SELECT empregado.nome FROM empregado  JOIN trabalha ON trabalha.codEmpregado = empregado.codEmpregado JOIN companhia ON companhia.codCompanhia = trabalha.codCompanhia WHERE companhia.nome!="TIM" and trabalha.salario >= 5000;
+SELECT empregado.nome FROM empregado 
+ JOIN trabalha ON trabalha.codEmpregado = empregado.codEmpregado 
+ JOIN companhia ON companhia.codCompanhia = trabalha.codCompanhia
+ WHERE companhia.nome!="TIM" and trabalha.salario >= 5000;
 
 f)
 SELECT empregado.nome FROM ( select empregado.nome, empregado.CodEmpregado from empregado 
@@ -24,35 +37,92 @@ SELECT empregado.nome FROM ( select empregado.nome, empregado.CodEmpregado from 
 g)
 SELECT * FROM companhia WHERE companhia.cidade = "Cianorte" ;
 
+h)
+SELECT empregado.nome, empregado.cidade FROM (select empregado.CodEmpregado, trabalha.codCompanhia FROM empregado
+JOIN gerente ON gerente.codEmpregado = empregado.CodEmpregado AND empregado.nome = 'Alexandre'
+JOIN trabalha ON empregado.CodEmpregado = trabalha.CodEmpregado
+JOIN companhia ON companhia.CodCompanhia = trabalha.CodCompanhia) t1
+JOIN trabalha ON trabalha.codCompanhia = t1.codCompanhia 
+JOIN empregado ON empregado.CodEmpregado = trabalha.CodEmpregado AND t1.CodEmpregado != empregado.CodEmpregado;
+
+i)
+SELECT empregado.nome, empregado.rua, empregado.cidade FROM (select trabalha.codEmpregado FROM trabalha 
+JOIN companhia ON trabalha.codCompanhia = companhia.codCompanhia AND companhia.cidade='Cianorte') t1 
+JOIN empregado ON empregado.CodEmpregado = t1.codEmpregado AND empregado.nome != 'Augusto';
+
 j)
 SELECT empregado.nome FROM empregado
  JOIN trabalha ON trabalha.CodEmpregado = empregado.CodEmpregado 
  JOIN companhia ON companhia.CodCompanhia = trabalha.CodCompanhia
  WHERE empregado.cidade = companhia.cidade;
 
+k)
+SELECT empregado.nome FROM (select empregado.CodEmpregado, empregado.rua, empregado.cidade FROM empregado 
+JOIN gerente ON gerente.CodEmpregado = empregado.codEmpregado) t1 
+JOIN empregado ON empregado.rua = t1.rua AND empregado.cidade = t1.cidade AND t1.codEmpregado != empregado.CodEmpregado;
 
-h)
+l)
+SELECT empregado.nome FROM (select cliente.cidade FROM cliente WHERE cliente.nome = 'João') t1 
+JOIN empregado ON empregado.cidade = t1.cidade;
+SELECT empregado.nome FROM (SELECT empregado.cidade FROM empregado WHERE empregado.nome = 'João') t2
+JOIN empregado ON empregado.cidade = t2.cidade;
+
+m)
+SELECT empregado.nome FROM empregado
+JOIN trabalha ON empregado.CodEmpregado = trabalha.codEmpregado 
+JOIN companhia ON companhia.CodCompanhia = trabalha.codCompanhia WHERE companhia.nome = 'Varig'
+UNION
+SELECT cliente.nome FROM cliente 
+WHERE cliente.cidade = 'Curitiba';
 
 
 n)
-SELECT empregado.nome FROM empregado  JOIN trabalha ON trabalha.codEmpregado = empregado.codEmpregado JOIN companhia ON companhia.codCompanhia = trabalha.codCompanhia WHERE companhia.nome="TAM" AND empregado.cidade!="São Paulo";
+SELECT empregado.nome FROM empregado 
+JOIN trabalha ON trabalha.codEmpregado = empregado.codEmpregado
+JOIN companhia ON companhia.codCompanhia = trabalha.codCompanhia
+WHERE companhia.nome="TAM" AND empregado.cidade!="São Paulo";
 
 o)
-SELECT empregado.nome FROM empregado  JOIN trabalha ON trabalha.codEmpregado = empregado.codEmpregado JOIN companhia ON companhia.codCompanhia = trabalha.codCompanhia WHERE companhia.nome="TAM" AND empregado.cidade="Maceió";
+SELECT empregado.nome FROM empregado
+JOIN trabalha ON trabalha.codEmpregado = empregado.codEmpregado 
+JOIN companhia ON companhia.codCompanhia = trabalha.codCompanhia
+WHERE companhia.nome="TAM" AND empregado.cidade="Maceió";
+
+p)
+
+q)
+
+r)
+
+s)
+SELECT empregado.nome, Min(trabalha.salario), companhia.nome FROM empregado 
+    JOIN trabalha ON empregado.codEmpregado = trabalha.codEmpregado 
+    JOIN companhia ON companhia.codCompanhia = trabalha.codCompanhia;
 
 t)
 SET SQL_SAFE_UPDATES = 0;
 UPDATE empregado SET cidade = 'Brasilia' WHERE nome = 'José';
 
 u) 
-SELECT empregado.nome, trabalha.salario*0.1+trabalha.salario FROM empregado JOIN trabalha ON  trabalha.codEmpregado = empregado.codEmpregado JOIN companhia ON companhia.codCompanhia = trabalha.codCompanhia WHERE companhia.nome="Varig";
+SELECT empregado.nome, trabalha.salario*0.1+trabalha.salario FROM empregado 
+JOIN trabalha ON  trabalha.codEmpregado = empregado.codEmpregado 
+JOIN companhia ON companhia.codCompanhia = trabalha.codCompanhia WHERE companhia.nome="Varig";
     
 v)
-select empregado.nome, trabalha.salario*0.03+trabalha.salario from empregado 
+SELECT empregado.nome, trabalha.salario*0.03+trabalha.salario FROM empregado 
 JOIN trabalha ON empregado.codEmpregado = trabalha.codEmpregado 
 JOIN companhia ON companhia.codCompanhia = trabalha.codCompanhia
 JOIN gerente ON gerente.codEmpregado = empregado.codEmpregado;
 
 w)
-select trabalha.codEmpregado, trabalha.salario*0.07+trabalha.salario from trabalha join gerente on gerente.codEmpregado = trabalha.codEmpregado WHERE trabalha.salario>=1200;
-select trabalha.codEmpregado, trabalha.salario*0.10+trabalha.salario from trabalha join gerente on gerente.codEmpregado != trabalha.codEmpregado WHERE trabalha.salario<=1200;
+SELECT trabalha.codEmpregado, trabalha.salario*0.07+trabalha.salario FROM trabalha JOIN gerente ON gerente.codEmpregado = trabalha.codEmpregado WHERE trabalha.salario>=1200;
+SELECT trabalha.codEmpregado, trabalha.salario*0.10+trabalha.salario FROM trabalha JOIN gerente ON gerente.codEmpregado != trabalha.codEmpregado WHERE trabalha.salario<=1200;
+
+x)
+DELETE FROM empregado WHERE empregado.nome = 'Jesus';
+
+y)
+
+
+z)
+
